ddc1600fd5cd950e9a63eb2455bafa6002671048
sriharsha <91383946+sriharsha8991@users.noreply.github.com>
2023-01-17 16:39:53 +0530

    Update Questions Daily Anova.py

--- Questions Daily Anova.py
+++ Questions Daily Anova.py
@@ -141,5 +141,83 @@ odd_op(pl)
 #output: [1, 3, 5, 7, 9]
 
 
+#_____________________________________________________________________________________________________________________________________________________________________
+#_______________________________________________________________________D A Y 2_______________________________________________________________________________________
+#-----------------------------------------------------------------------------Q1--------------------------------------------------------------------------------------
+### Q1
+#My Question--
+#"Given a list of integers, write a function to find the second largest number in the list.
+#The function should return the second largest number, or None if there is less than 2 numbers in the list. 
+#Additionally, the function should not use any built-in sorting functions, and should have a time complexity of O(n)."
 
+#Method 1
+pp = [1,2,3,4,56,67,5]
+def second_largest(nums):
+    if len(nums)>2:
+        nums.remove(max(nums))
+        return max(nums)
+    return None
+#second_largest(pp)
 
+#Method 2
+maxi = 0
+second = 0
+for i in pp: 
+    if i>maxi:
+        maxi,second = i,maxi
+    elif i>second:
+        second = i
+print(second)
+
+#Method sorting -3
+P_sort = sorted(pp)
+second_largest = P_sort[-2]
+second_largest
+#Output -- 56
+
+#----------------------------------------------------------------------Q2--------------------------------------------------------------------------------------------
+### Q2
+# Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+
+# wins get 3 points
+# draws get 1 point
+# losses get 0 points
+
+# Examples
+# football_points(3, 4, 2) ➞ 13
+# football_points(5, 0, 2) ➞ 15
+# football_points(0, 0, 1) ➞ 0
+# Inputs will be numbers greater than or equal to 0.
+
+def football_points(tup):
+    #tup-->(win,draw,lost)
+    """ wins get 3 points
+        draws get 1 point
+        losses get 0 points
+        """
+    return tup[0]*3+tup[1]*1
+football_points((5,0,2))
+#Output -- 15
+
+#-----------------------------------------------------------------Q3------------------------------------------------------------------------------------------------
+### Q3
+# Create a python program that takes a string as input and returns a new string with the first and last letters of the input string swapped.
+
+# You should use string slicing to solve this problem.
+
+# Example:
+def swap_first_last(string):
+    # return the new string with first and last letters swapped
+    pass
+
+# print(swap_first_last("Hello")) # should return "oellH"
+# This problem requires the use of string slicing and string manipulation to swap the first and last letters of the input string.
+# It's a simple problem that can help students to practice their understanding of string slicing and string manipulation in python.
+
+def swap_first_last(string):
+    return string[-1]+string[1:-1]+string[0]
+swap_first_last("Hello")
+#Output-- 'oellH'
+
+#______________________________________________________________________________________________________________________________________________________________________
+#-----------------------------------------------------------------------DAY3-------------------------------------------------------------------------------------------
